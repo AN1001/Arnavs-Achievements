6e69ef3ed23d090fdd124d29c7599ed8854f271a
AN1001 <81749717+AN1001@users.noreply.github.com>
2021-07-19 18:28:51 +0000

    Update buttonsA.js

--- buttonsA.js
+++ buttonsA.js
@@ -1,13 +1,15 @@
 /*Picture buttons*/
-document.getElementById("cssimg").onclick = function() {myFunction('csssite.html')};
-document.getElementById("PYRL-img").onclick = function() {myFunction('pysite.html')};
-document.getElementById("JSuntagged").onclick = function() {myFunction('javasite.html')};
-document.getElementById("Unity-img").onclick = function() {myFunction('unity.html')};
-document.getElementById("HTMlp-img").onclick = function() {myFunction('htmlsite.html')};
+document.getElementById("cssimg").onclick = function() {redirect('csssite.html')};
+document.getElementById("PYRL-img").onclick = function() {redirect('pysite.html')};
+document.getElementById("JSuntagged").onclick = function() {redirect('javasite.html')};
+document.getElementById("Unity-img").onclick = function() {redirect('unity.html')};
+document.getElementById("HTMlp-img").onclick = function() {redirect('htmlsite.html')};
 
-function myFunction(name) {
+function redirect(name) {
   window.location.href = name;
 }
 
 /*Dropdown menu buttons*/
-document.getElementById("in-bord").onclick = function() {myFunction('htmlsite.html')};
+document.getElementById("ba").onclick = function() {redirect('htmlsite.html')};
+document.getElementById("bb").onclick = function() {redirect('htmlsite.html')};
+document.getElementById("bc").onclick = function() {redirect('htmlsite.html')};
