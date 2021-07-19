509a602a211e8322e4e1803eb48908275ad1ea34
AN1001 <81749717+AN1001@users.noreply.github.com>
2021-07-19 13:40:26 +0000

    Update buttonsA.js

--- buttonsA.js
+++ buttonsA.js
@@ -1,4 +1,8 @@
 document.getElementById("cssimg").onclick = function() {myFunction('csssite.html')};
+document.getElementById("PYRL-img").onclick = function() {myFunction('pysite.html')};
+document.getElementById("JSuntagged").onclick = function() {myFunction('javasite.html')};
+document.getElementById("Unity-img").onclick = function() {myFunction('unity.html')};
+document.getElementById("HTMlp-img").onclick = function() {myFunction('htmlsite.html')};
 
 function myFunction(name) {
   window.location.href = name;
