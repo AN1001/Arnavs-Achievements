de29a47817d611b627a2e2e404ae89ba691c831f
AN1001 <81749717+AN1001@users.noreply.github.com>
2021-07-19 10:54:38 +0000

    Update buttonsA.js

--- buttonsA.js
+++ buttonsA.js
@@ -2,4 +2,5 @@ document.getElementById("cssimg").onclick = function() {myFunction()};
 
 function myFunction() {
   document.getElementById("cssimg").innerHTML = "YOU CLICKED ME!";
+  location.replace("https://www.w3schools.com")
 }
