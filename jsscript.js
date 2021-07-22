6d606901f4cd480b48b368730efd86d6e3357039
AN1001 <81749717+AN1001@users.noreply.github.com>
2021-07-22 13:46:39 +0000

    Update jsscript.js

--- jsscript.js
+++ jsscript.js
@@ -1,6 +1,6 @@
 const colours = ["red", "whitesmoke", "rgb(133,115,45)","#f15025"];
 const btn = document.getElementById('btn')
-const color = document.getElementById('colour-name')
+const colour = document.getElementById('colour-name')
 
 btn.addEventListener('click',function(){
   const randomNum = 2;
