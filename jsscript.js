e9e88ba9417a93e0b797a4de268ccdde8382fcb1
AN1001 <81749717+AN1001@users.noreply.github.com>
2021-07-22 13:23:14 +0000

    Update jsscript.js

--- jsscript.js
+++ jsscript.js
@@ -1,1 +1,7 @@
+const colours = ["red", "whitesmoke", "rgb(133,115,45)","#f15025"];
+const btn = document.getElementById('btn')
+const color = document.getElementById('colour-name')
 
+btn.addEventListener('click',function(){
+  console.log(document.body)
+})
