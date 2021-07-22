ab547989a745953a61be1337559124d66ed96899
AN1001 <81749717+AN1001@users.noreply.github.com>
2021-07-22 19:08:51 +0000

    Update jsscript.js

--- jsscript.js
+++ jsscript.js
@@ -3,22 +3,22 @@
 project 1, colour switcher
 ===========================
 */
-const hexElements = ["1","2","3","4","5","6","A","B","C","D","E","F"];
+const hexElements = ["1", "2", "3", "4", "5", "6", "A", "B", "C", "D", "E", "F"];
 const btn = document.getElementById('btn');
 const colour = document.getElementById('colour-name');
 
-btn.addEventListener('click',function(){
+btn.addEventListener('click', function() {
   var hexColour = []
-  for(let i = 0;i<6;i++){
-    hexColour.push( hexElements[getRandomNumber(hexElements.length)] )
+  for (let i = 0; i < 6; i++) {
+    hexColour.push(hexElements[getRandomNumber(hexElements.length)])
   }
-  
-  var joinedHex = "#"+hexColour.join("")
+
+  var joinedHex = "#" + hexColour.join("")
   document.getElementById("target1").style.backgroundColor = joinedHex;
   colour.textContent = joinedHex;
 })
 
-function getRandomNumber(max){
+function getRandomNumber(max) {
   return Math.floor(Math.random() * max);
 }
 
@@ -33,30 +33,29 @@ let count = 0;
 const value = document.querySelector('#span');
 const btns = document.querySelectorAll('.btn');
 
-btns.forEach(function(item){
-  item.addEventListener("click", function(e){
+btns.forEach(function(item) {
+  item.addEventListener("click", function(e) {
     const styles = e.currentTarget.classList;
-    
-    if(styles.contains('inc')){
+
+    if (styles.contains('inc')) {
       count++;
     }
-    if(styles.contains('dec')){
+    if (styles.contains('dec')) {
       count--;
     }
-    if(styles.contains('reset')){
+    if (styles.contains('reset')) {
       count = 0;
     }
-    
-    
-    if(count>0){
+
+
+    if (count > 0) {
       value.style.color = 'green';
-    } else if(count<0){
+    } else if (count < 0) {
       value.style.color = 'red';
     } else {
       value.style.color = 'black';
     }
     value.textContent = count;
   })
-    
-})
 
+})
