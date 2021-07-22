832df205e29b2e87e79075cda493b3ca89827ddc
AN1001 <81749717+AN1001@users.noreply.github.com>
2021-07-22 13:54:07 +0000

    Update jsscript.js

--- jsscript.js
+++ jsscript.js
@@ -3,7 +3,12 @@ const btn = document.getElementById('btn')
 const colour = document.getElementById('colour-name')
 
 btn.addEventListener('click',function(){
-  const randomNum = 2;
+  const randomNum = getRandomNumber(colours.length);
+  
   document.getElementById("target1").style.backgroundColor = colours[randomNum];
   colour.textContent = colours[randomNum]
 })
+
+function getRandomNumber(max){
+  return Math.floor(Math.random() * max);
+}
