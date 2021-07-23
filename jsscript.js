62b193f5305035df422f2163cac3c8c0c4db5289
AN1001 <81749717+AN1001@users.noreply.github.com>
2021-07-23 18:32:49 +0000

    Update jsscript.js

--- jsscript.js
+++ jsscript.js
@@ -69,4 +69,10 @@ Project 3, Complex Polygon Maker
 const shapes = [
   {name:"Octogon",func:"clip-path: polygon(30% 0%, 70% 0%, 100% 30%, 100% 70%, 70% 100%, 30% 100%, 0% 70%, 0% 30%);"},
   {name: "Heptagon",func:"clip-path: polygon(50% 0%, 90% 20%, 100% 60%, 75% 100%, 25% 100%, 0% 60%, 10% 20%);"}
-]
+];
+const shape = document.querySelector("shape");
+const shapeName = document.getElementById("shapeName");
+const prevBtn = document.getElementById("prev");
+const nextBtn = document.getElementById("next");
+var current = 0;
+
