33a6aa9331751266787813f7802651cd0f7af923
AN1001 <81749717+AN1001@users.noreply.github.com>
2021-07-24 10:12:03 +0000

    Update jsscript.js

--- jsscript.js
+++ jsscript.js
@@ -67,8 +67,8 @@ Project 3, Complex Polygon Maker
 */
 
 const shapes = [
-  {id:1, name:"Octogon",func:"clip-path: polygon(30% 0%, 70% 0%, 100% 30%, 100% 70%, 70% 100%, 30% 100%, 0% 70%, 0% 30%);"},
-  {id:2, name: "Heptagon",func:"clip-path: polygon(50% 0%, 90% 20%, 100% 60%, 75% 100%, 25% 100%, 0% 60%, 10% 20%);"}
+  {id:1, name:"Octogon",func:"polygon(30% 0%, 70% 0%, 100% 30%, 100% 70%, 70% 100%, 30% 100%, 0% 70%, 0% 30%)"},
+  {id:2, name: "Heptagon",func:"polygon(50% 0%, 90% 20%, 100% 60%, 75% 100%, 25% 100%, 0% 60%, 10% 20%)"}
 ];
 const shape = document.querySelector(".shape");
 const shapeName = document.getElementById("shapeName");
