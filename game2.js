0d32f08da7b80f477e6bb042fa5160657a66b0d4
AN1001 <81749717+AN1001@users.noreply.github.com>
2021-08-04 15:15:43 +0100

    Update game2.js

--- game2.js
+++ game2.js
@@ -2,7 +2,7 @@ const player = document.createElement("div")
 const gameboard = document.querySelector(".game2MainGridSpace")
 var playerPosition = 6;
 
-player.classList.add("player2")
 player.style.gridRowStart = 11;
+player.classList.add("player2")
 player.style.gridRowStart = playerPosition;
 gameboard.appendChild(player)
