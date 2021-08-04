02310fd9b90583a6072db4db20d91140216944d6
AN1001 <81749717+AN1001@users.noreply.github.com>
2021-08-04 15:10:09 +0100

    Update game2.js

--- game2.js
+++ game2.js
@@ -1,1 +1,7 @@
+const player = document.createElement("div")
+const gameboard = document.querySelector(".game2MainGridSpace")
+var playerPosition = 6;
 
+player.style.gridRowStart = 11;
+player.style.gridRowStart = playerPosition;
+gameboard.appendChild(player)
