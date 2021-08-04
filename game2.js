b7ed3a8aacf39a374d73b3a4556d8a7b003ddab5
AN1001 <81749717+AN1001@users.noreply.github.com>
2021-08-04 17:27:02 +0100

    Update game2.js

--- game2.js
+++ game2.js
@@ -1,13 +1,12 @@
-const player = document.createElement("div")
-const gameboard = document.querySelector(".game2MainGridSpace")
+const player = document.createElement("div");
+const gameboard = document.querySelector(".game2MainGridSpace");
 const gameTickSpeed = 1000;
 let lastRenderTime = 0;
 var playerPosition = 11;
 
 player.style.gridRowStart = 21;
-player.classList.add("player2")
-player.style.gridColumnStart = playerPosition;
-gameboard.appendChild(player)
+player.classList.add("player2");
+
 
 //Button inputs
 gameboard.addEventListener('click',function(){
@@ -18,6 +17,10 @@ function mainLoop(currentTime){
   window.requestAnimationFrame(mainLoop);
   let lastTime = (currentTime - lastRenderTime);
   if (lastTime < gameTickSpeed) return;
+  
+  playerPosition++;
+  player.style.gridColumnStart = playerPosition;
+  //gameboard.appendChild(player);
   
   console.log("render occured");
   lastRenderTime = currentTime;
