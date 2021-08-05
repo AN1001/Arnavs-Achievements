6d498bd6b50b2a48dee715644b4e11ed8a771a8e
AN1001 <81749717+AN1001@users.noreply.github.com>
2021-08-05 20:20:22 +0100

    Update game2.js

--- game2.js
+++ game2.js
@@ -61,6 +61,7 @@ function mainLoop(currentTime){
   
   bullets.forEach(function(bullet){
     const currentPos = parseInt(window.getComputedStyle(bullet).getPropertyValue("gridRowStart"));
+    console.log(currentPos)
     if(currentPos > 1){ bullet.gridRowStart = currentPos-1; }
   });
   
