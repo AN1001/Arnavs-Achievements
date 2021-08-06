d394740ea976c7bd9a3a9369972f97d65c96fab4
AN1001 <81749717+AN1001@users.noreply.github.com>
2021-08-06 12:24:49 +0100

    Update game2.js

--- game2.js
+++ game2.js
@@ -10,7 +10,7 @@ var enemyShouldMove = 0;
 var currentIt = 0;
 var playerPosition = 11;
 var bullets = [];
-var enemyMovementPattern = [{x:-1,y:0},{x:1,y:0},{x:1,y:0},{x:-1,y:0},{x:0,y:-1}]
+var enemyMovementPattern = [{x:-1,y:0},{x:1,y:0},{x:1,y:0},{x:-1,y:0},{x:0,y:1}]
 var enemies = [
   {x:2,y:2},{x:4,y:2},{x:6,y:2},{x:8,y:2},{x:10,y:2},{x:12,y:2},{x:14,y:2},{x:16,y:2},{x:18,y:2},{x:20,y:2}
   ,{x:2,y:4},{x:4,y:4},{x:6,y:4},{x:8,y:4},{x:10,y:4},{x:12,y:4},{x:14,y:4},{x:16,y:4},{x:18,y:4},{x:20,y:4}
