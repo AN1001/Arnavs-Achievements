6d45649802ed7da1728f044ef321553e8b8e9c15
AN1001 <81749717+AN1001@users.noreply.github.com>
2022-02-19 19:16:35 +0000

    Update game4.js

--- game4.js
+++ game4.js
@@ -4,16 +4,16 @@ var width = window.innerWidth;
 var playingGame4 = true;
 var time = 0;
 
-aimSpace.style.display = "none";
-errorSpace.style.display = "block";
 console.log(width)
 
 setInterval(function(){
   if(playingGame4){time++;}
-  if(width < 460){
+  if(width < 1000){
+    width = window.innerWidth;
     aimSpace.style.display = "none";
     errorSpace.style.display = "block";
-  }else if(false){
+  }else{
+    width = window.innerWidth;
     aimSpace.style.display = "block";
     errorSpace.style.display = "none";
   }
