990a6b5d1cfa29b3801e44fc976295883685711d
AN1001 <81749717+AN1001@users.noreply.github.com>
2022-02-19 19:02:11 +0000

    Update game4.js

--- game4.js
+++ game4.js
@@ -1,1 +1,16 @@
+const aimSpace = document.getElementById("game4AimSpace");
+const errorSpace = document.getElementById("decoyAimSpace");
+var playingGame4 = true;
+var time = 0;
 
+
+setInterval(function(){
+  if(playingGame4){time++;}
+  if(window.innerWidth < 460){
+    aimSpace.style.display = "none";
+    errorSpace.style.display = "block";
+  }else{
+    aimSpace.style.display = "block";
+    errorSpace.style.display = "none";
+  }
+},500);
