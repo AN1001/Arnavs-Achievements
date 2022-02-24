8d7621d2e911fed0d28edfec19e4c9556c16d9db
AN1001 <81749717+AN1001@users.noreply.github.com>
2022-02-24 19:49:46 +0000

    Update game4.js

--- game4.js
+++ game4.js
@@ -1,6 +1,8 @@
 const aimSpace = document.getElementById("game4AimSpace");
 const errorSpace = document.getElementById("decoyAimSpace");
 let playingGame4 = true;
+let divWidth = aimSpace.offsetWidth;
+let divHeight = aimSpace.offsetHeight;
 var width = window.innerWidth;
 var height = window.innerHeight;
 var game4time = 0;
@@ -24,8 +26,6 @@ setInterval(function(){
 
 
 setInterval(function(){
-  let divWidth = aimSpace.offsetWidth;
-  let divHeight = aimSpace.offsetHeight;
   var target = document.createElement("div")
   target.classList.add("target")
   aimSpace.appendChild(target)
