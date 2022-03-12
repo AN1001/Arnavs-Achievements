1babbf21dc57fb22adac2ff365a8fd4eb6613c66
AN1001 <81749717+AN1001@users.noreply.github.com>
2022-03-12 18:32:42 +0000

    Update game4.js

--- game4.js
+++ game4.js
@@ -1,10 +1,11 @@
 const aimSpace = document.getElementById("game4AimSpace");
 const errorSpace = document.getElementById("decoyAimSpace");
 let playingGame4 = true;
+let shouldSpawn = true;
 var width = window.innerWidth;
 var height = window.innerHeight;
 var game4time = 0;
-var activeTargets = []
+
 
 function randomInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min)
@@ -15,26 +16,26 @@ setInterval(function(){
   if(width < 1000){
     width = window.innerWidth;
     aimSpace.style.display = "none";
-    errorSpace.style.display = "block";
-    for (let i2 = 0; i2 < activeTargets.length; i2++) {
-      activeTargets[i2].remove()
-    };
+    shouldSpawn = false;
     
   }else{
     width = window.innerWidth;
     aimSpace.style.display = "block";
     errorSpace.style.display = "none";
+    shouldSpawn = true;
+    
   }
 },1000);
 
 
 setInterval(function(){
-  var target = document.createElement("div")
-  let divWidth = aimSpace.offsetWidth;
-  let divHeight = aimSpace.offsetHeight;
-  
-  activeTargets.push(target)
-  target.classList.add("target")
-  aimSpace.appendChild(target)
-  target.style.transform = `translate(${randomInt(0,divWidth-100)}px,${randomInt(0,divHeight-100)}px)`;
+  if (shouldSpawn){
+    var target = document.createElement("div")
+    let divWidth = aimSpace.offsetWidth;
+    let divHeight = aimSpace.offsetHeight;
+
+    target.classList.add("target")
+    aimSpace.appendChild(target)
+    target.style.transform = `translate(${randomInt(0,divWidth-100)}px,${randomInt(0,divHeight-100)}px)`;
+  }
 },3000);
