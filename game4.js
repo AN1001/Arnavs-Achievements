0d43a7e20e9a442dc5df06f95a3ca646498f5d50
AN1001 <81749717+AN1001@users.noreply.github.com>
2022-03-12 18:14:01 +0000

    Update game4.js

--- game4.js
+++ game4.js
@@ -4,6 +4,7 @@ let playingGame4 = true;
 var width = window.innerWidth;
 var height = window.innerHeight;
 var game4time = 0;
+var activeTargets = []
 
 function randomInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min)
@@ -15,6 +16,10 @@ setInterval(function(){
     width = window.innerWidth;
     aimSpace.style.display = "none";
     errorSpace.style.display = "block";
+    for (let i = 0, i < activeTargets.length, i++) {
+      activeTargets[0].remove()
+    };
+    
   }else{
     width = window.innerWidth;
     aimSpace.style.display = "block";
@@ -27,6 +32,8 @@ setInterval(function(){
   var target = document.createElement("div")
   let divWidth = aimSpace.offsetWidth;
   let divHeight = aimSpace.offsetHeight;
+  
+  activeTargets.push(target)
   target.classList.add("target")
   aimSpace.appendChild(target)
   target.style.transform = `translate(${randomInt(0,divWidth-100)}px,${randomInt(0,divHeight-100)}px)`;
